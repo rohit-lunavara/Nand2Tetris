349dfa759a5d2b5b2a6909b09b980ad6c91272ee
rohit-lunavara <rohit.lunavara@gmail.com>
2020-04-05 23:00:02 -0400

    Project 4 done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// LOOP :
+//   if KBD != 0 goto FILL
+//   reset SCREEN
+//   goto LOOP
+//   
+// FILL :
+//   set SCREEN
+//   goto LOOP
+
+@8192
+D = A    // D = total number of pixels
+@blocks
+M = D    // blocks = 512
+
+(LOOP)
+	@KBD
+	D = M
+	@FILL
+	D ; JNE // if KBD != 0 goto FILL
+
+	@n_block
+	M = 0 // n_block = 0
+
+	(EMPTYSCREEN)
+		@n_block
+		D = M
+		@blocks
+		D = D - M
+		@LOOP
+		D ; JEQ // if n_block - blocks == 0 goto LOOP
+
+		@n_block
+		D = M // D = n_block
+		@SCREEN
+		A = A + D
+		M = 0 // reset SCREEN[n_block]
+
+		@n_block
+		M = M + 1 // n_block = n_block + 1
+
+		@EMPTYSCREEN
+		0 ; JMP // goto EMPTYSCREEN
+
+	@LOOP
+	0 ; JMP // goto LOOP
+
+(FILL)
+	@SCREEN
+	M = -1 // set SCREEN
+
+	@n_block
+	M = 0 // n_block = 0
+
+	(FILLSCREEN)
+		@n_block
+		D = M
+		@blocks
+		D = D - M
+		@LOOP
+		D ; JEQ // if n_block - blocks == 0 goto LOOP
+
+		@n_block
+		D = M
+		@SCREEN
+		A = A + D
+		M = -1 // set SCREEN[n_block]
+
+		@n_block
+		M = M + 1 // n_block = n_block + 1
+
+		@FILLSCREEN
+		0 ; JMP // goto FILLSCREEN
+
+	@LOOP
+	0 ; JMP // goto LOOP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,36 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// R2 = 0
+// LOOP :
+//   if R0 == 0 goto END
+//   R2 = R2 + R1
+//   R0 = R0 - 1
+//   goto LOOP
+// END :
+//   Infinite Loop
+
+@R2
+M = 0 // R2 = 0
+
+(LOOP)
+	@R0
+	D = M
+	@END
+	D ; JEQ // if R0 == 0 goto END
+
+	@R1
+	D = M // D = R1
+	@R2
+	M = M + D // R2 = R2 + R1
+
+	@R0
+	M = M - 1 // R0 = R0 - 1
+
+	@LOOP
+	0 ; JMP // goto LOOP
+
+(END)
+	@END
+	0 ; JMP // Infinite Loop
